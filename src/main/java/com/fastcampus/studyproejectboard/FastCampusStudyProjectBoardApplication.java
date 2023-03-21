113ad4219766beda3a71239c7da5c51fc4fae761
Clustern <homerun372@gmail.com>
2023-03-22 02:00:31 +0900

    #6 - 메서드 오타 수정
    IDE에서 감지되고 수정 제안이 발생했으며 이를 수용

--- src/main/java/com/fastcampus/studyproejectboard/FastCampusStudyProjectBoardApplication.java
+++ src/main/java/com/fastcampus/studyproejectboard/FastCampusStudyProjectBoardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FastcampusStudyProjectBoardApplication {
+public class FastCampusStudyProjectBoardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FastcampusStudyProjectBoardApplication.class, args);
+        SpringApplication.run(FastCampusStudyProjectBoardApplication.class, args);
     }
 
 }
