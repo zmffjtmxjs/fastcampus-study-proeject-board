a012adcfc307f892dbb1f1cf44e53d41023b5ac3
Clustern <homerun372@gmail.com>
2023-03-22 01:44:03 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    추가된 종속성
    * spring web
    * spring atuator
    * spring devtools
    * lombok

---
+++ src/main/java/com/fastcampus/studyproejectboard/FastcampusStudyProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.studyproejectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusStudyProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusStudyProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/studyproejectboard/FastcampusStudyProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.studyproejectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusStudyProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
