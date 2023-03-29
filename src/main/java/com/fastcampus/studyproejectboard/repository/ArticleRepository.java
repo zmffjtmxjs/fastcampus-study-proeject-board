bff8415b4afe09076f80fc4fc760b275dfc19e1d
Clustern <homerun372@gmail.com>
2023-03-29 16:09:30 +0900

    #1 - Querydsl 지원을 이용한 api 검색 옵션 추가 `QuerydslPredicateExecutor`, `QuerydslBinderCustomizer`을 활용하여 간단히 구현

--- src/main/java/com/fastcampus/studyproejectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/studyproejectboard/repository/ArticleRepository.java
@@ -1,9 +1,29 @@
 package com.fastcampus.studyproejectboard.repository;
 
 import com.fastcampus.studyproejectboard.domain.Article;
+import com.fastcampus.studyproejectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); //TODO : 인증 기능(로그인) 추가 후 ID 기준 검색 추가
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+
+    }
 }
